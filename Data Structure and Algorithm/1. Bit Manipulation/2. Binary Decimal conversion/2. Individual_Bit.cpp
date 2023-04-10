02cdf12df53fb879f3a0b7be4a4215ea055b755f
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2023-04-10 20:25:31 +0530

    Individual Bit of number

---
+++ Data Structure and Algorithm/1. Bit Manipulation/2. Binary Decimal conversion/2. Individual_Bit.cpp
@@ -0,0 +1,39 @@
+// Q. Print all Individual Bit of a given number and print it on Console.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+   
+
+
+     int no;
+
+    cout << "\n Enter any Number :";
+    cin >> no;
+    cout << endl;
+
+    int temp = no;
+    int ans = 0, r = 0;
+    while (temp != 0)
+    {
+        r = temp % 10;
+        ans = ans * 10 + r;
+        temp = temp / 10;
+    }
+
+    no = ans;  
+
+    cout << "\n The Individual Bit of Number " << no << " is :" << endl;
+    while (no)
+    {
+        r = no % 10;
+        no = no / 10;
+        ans =r ;
+        cout << ans << endl;
+    }
+
+
+    return 0;
+}
